9b6629d39100568e9095ca2d79a6fd864f97703b
Amadou-Marong <marongamadou6@gmail.com>
2025-02-26 13:39:55 +0000

    scroll behaviour

---
+++ PersonalPortfolioExamples/Portfolio/script.js
@@ -0,0 +1,11 @@
+// const menuIcon = document.getElementById('menu-icon');
+const openMenuIcon = document.getElementById('open')
+const closeMenuIcon = document.getElementById('close')
+
+const mobileNav = document.getElementById('mobile-nav');
+
+
+
+menuIcon.addEventListener('click', () => {
+
+});
